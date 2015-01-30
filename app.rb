dc690f44444673c9bb7710d76c5295244c83e767
Eric Sorrells <ericsorrells@hotmail.com>
2015-01-30 13:41:36 -0500

    local commit

--- app.rb
+++ app.rb
@@ -1,6 +1,22 @@
 require 'sinatra'
+require 'pry'
 require_relative './model/projects'
 
 get '/' do
   erb :home
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/projects' do
+  @projects = Projects.all
+  erb :projects
+end
+
+get '/projects/:id' do
+  @project = Projects.find(params[:id].to_i)
+  puts "project = #{@project}"
+  erb :project
 end
